1906b187e538627f79ec82b43b9f66f2d7a984bf
Nicolas Sutter <contact.sutter.nicolas@gmail.com>
2023-01-04 11:31:09 +0100

    refactor: import css as inline

--- src/lib.ts
+++ src/lib.ts
@@ -6,14 +6,15 @@ export async function applyTheme(_theme: 'catppuccin-macchiato') {
   if (typeof document !== 'undefined') {
     init()
 
-    const availableThemes = import.meta.glob('./themes/*.css')
+    const availableThemes = import.meta.glob('./themes/*.css', { query: '?inline' })
+
     const theme = Object.entries(availableThemes)
       .find(([key]) => key.endsWith(`${_theme}.css`))
 
     if (theme) {
       const [, themeImportFn] = theme
-      const css = await themeImportFn() as string
-  
+      const { default: css } = await themeImportFn() as { default: string }
+
       const style = document.createElement('style')
       style.innerText = css
   
