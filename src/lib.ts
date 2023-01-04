35e824496562f74b8996aa41388e32260d0a7b9a
Nicolas Sutter <contact.sutter.nicolas@gmail.com>
2023-01-04 18:54:04 +0100

    feat: add attribute to style tag

--- src/lib.ts
+++ src/lib.ts
@@ -3,6 +3,12 @@ import 'uno.css'
 
 function init() {
   document.body.setAttribute('data-netlifycms-theme', '')
+
+  const existingStyles = document.querySelector('data-netlifycms-styles')
+
+  if (existingStyles) {
+    existingStyles.parentElement?.removeChild(existingStyles)
+  }
 }
 
 export async function applyTheme(_theme: Theme) {
@@ -19,6 +25,7 @@ export async function applyTheme(_theme: Theme) {
       const { default: css } = await themeImportFn() as { default: string }
 
       const style = document.createElement('style')
+      style.setAttribute('data-netlifycms-styles', '')
       style.innerText = css
   
       document.head.appendChild(style)
